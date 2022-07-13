22659f39dbc7c40d802ae17a0ec329504f744b49
JasonGeee <Jasongeee@yahoo.com>
2022-07-13 15:07:02 -0700

    created footer and navbar

---
+++ src/pages/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='home'>
+      <div className="about">
+        <h2>Hi, My Name is Jason</h2>
+        <div className="prompt">
+          <p>
+            Aspiring Software Engineer with a passion for learning and creating.
+          </p>
+        </div>
+      </div>
+      <div className="skills"></div>
+    </div>
+  )
+}
+
+export default Home
